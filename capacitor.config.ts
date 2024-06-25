e96e1e47e34305fcbda37b5db19533f7246e244c
Ivan Ruiz <ivan.ruiz@peoplebpo.com>
2024-06-25 10:11:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ComponentesUI',
+  webDir: 'dist'
+};
+
+export default config;
